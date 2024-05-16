14dad1d488f1196cd0532812515beea3064493ec
seochae523 <seochae523@naver.com>
2024-05-16 16:09:00 +0900

    Refactor : refactor filter chain

--- src/main/java/sejongZoo/sejongZoo/common/configuration/SecurityConfig.java
+++ src/main/java/sejongZoo/sejongZoo/common/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,19 +28,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
-                .httpBasic().disable()
-                .csrf().disable()
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sesstionManagement -> sesstionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // 세션을 사용하지 않기 때문에 STATELESS로 설정
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers( "/find-nickname", "/login", "/signup", "/swagger-ui/**", "/v3/api-docs/**", "/check-nickname",
-                        "/check-student-id", "/change-forget-password", "/refresh","/ws/chat"
-                ).permitAll()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/user/**").hasRole("USER")
-
-                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers( "/find-nickname", "/login", "/signup", "/swagger-ui/**", "/v3/api-docs/**", "/check-nickname",
+                                "/check-student-id", "/change-forget-password", "/refresh","/ws/chat"
+                        ).permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasRole("USER")
+                )
                 .addFilterBefore(new JwtFilter(authTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
