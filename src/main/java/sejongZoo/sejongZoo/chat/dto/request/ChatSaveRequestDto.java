28e4e335f37981f2cf2daa978ad61013eaa5563e
seochae523 <seochae523@naver.com>
2024-06-12 02:17:40 +0900

    Docs : add dto's schema

--- src/main/java/sejongZoo/sejongZoo/chat/dto/request/ChatSaveRequestDto.java
+++ src/main/java/sejongZoo/sejongZoo/chat/dto/request/ChatSaveRequestDto.java
@@ -1,5 +1,6 @@
 package sejongZoo.sejongZoo.chat.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -13,12 +14,15 @@ import lombok.NoArgsConstructor;
 public class ChatSaveRequestDto {
     @NotNull(message = "Chat Room Id Not Found.")
     @PositiveOrZero(message = "Chat Room Id Must Be Positive Or Zero Value.")
+    @Schema(description = "채팅 방 번호", example = "1")
     private Long roomId;
 
     @NotBlank(message = "Chat Sender Not Found.")
+    @Schema(description = "작성자", example = "admin")
     private String sender;
 
     @NotBlank(message = "Chat Message Not Found.")
+    @Schema(description = "채팅 메시지", example = "hello world!")
     private String message;
 
 }
--- src/main/java/sejongZoo/sejongZoo/fcm/dto/FcmSendDto.java
+++ src/main/java/sejongZoo/sejongZoo/fcm/dto/FcmSendDto.java
@@ -1,6 +1,7 @@
 package sejongZoo.sejongZoo.fcm.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,10 +10,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Builder
 public class FcmSendDto {
+    @NotNull(message = "Fcm Token Not Found.")
     @Schema(description = "프론트에서 발급받은 기기 식별용 토큰", example = "something-token-code-in-here")
     private String token;
+    @NotNull(message = "Fcm Title Not Found.")
     @Schema(description = "알림 제목", example = "something title")
     private String title;
+    @NotNull(message = "Fcm context Not Found.")
     @Schema(description = "알림 내용", example = "something content")
     private String body;
 }
