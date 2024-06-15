55e3c0382dacd40621a2399648af860d61731e5a
seochae523 <seochae523@naver.com>
2024-06-16 04:23:49 +0900

    Fix : fix mail sender

--- src/main/java/sejongZoo/sejongZoo/user/service/impl/EmailSendServiceImpl.java
+++ src/main/java/sejongZoo/sejongZoo/user/service/impl/EmailSendServiceImpl.java
@@ -2,7 +2,9 @@ package sejongZoo.sejongZoo.user.service.impl;
 
 
 import lombok.RequiredArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
@@ -17,12 +19,15 @@ import sejongZoo.sejongZoo.user.service.EmailSendService;
 @Transactional(readOnly = true)
 public class EmailSendServiceImpl implements EmailSendService {
     private final MailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String host;
     @Override
     @Transactional
     public EmailSendResponseDto sendEmail(EmailSendRequestDto emailSendRequestDto) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("SejongUsedBookMarket@gmail.com");
-        simpleMailMessage.setTo("SejongUsedBookMarket@gmail.com");
+        simpleMailMessage.setFrom(host + "@gmail.com");
+        simpleMailMessage.setTo(host + "@gmail.com");
         simpleMailMessage.setSubject(emailSendRequestDto.getTitle());
         simpleMailMessage.setText(emailSendRequestDto.getContent() + "\n신고 게시글 Id: " + emailSendRequestDto.getBoardId());
         mailSender.send(simpleMailMessage);
